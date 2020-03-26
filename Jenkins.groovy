7d9b2117a9b45269fe3c868c42e68481bf59472b
ampandey24 <62716839+ampandey24@users.noreply.github.com>
2020-03-27 00:01:51 +0530

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,9 +1,15 @@
+def pros
 node 
 {
     stage('Ceckout SCM'){
         checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/ampandey24/JenkinsPipeline.git']]])   
         workspace = pwd()
         
+    }
+    stage ('Reading Properties File'){
+        pros = readProperties file:'login.propties'
+        url = $pros['URL']
+        echo ${url}
     }
     stage ('Build'){
         echo "Build"
