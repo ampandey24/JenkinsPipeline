4b12cce61c8638852e68ec22b57dde70a6cd8c78
ampandey24 <62716839+ampandey24@users.noreply.github.com>
2020-03-26 23:41:32 +0530

    Update Jenkins.groovy

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,2 +1,17 @@
+node 
 {
+    stage('Ceckout SCM'){
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/ampandey24/JenkinsPipeline.git']]])   
+        workspace = pwd()
+        
+    }
+    stage ('Build'){
+        echo "Build"
+    }
+    stage ('Test'){
+        echo "Test"
+    }
+    stage ('Deploy'){
+        echo "Deploy"
+    }
 }
